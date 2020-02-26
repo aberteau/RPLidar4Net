49f33a29aa37e3a55597245753efc952d8114463
Amael BERTEAU <amael.berteau@tech-easy.fr>
2020-02-26 19:00:45 +0100

    Refactoring ScanDataResponse (Ajout propriétés + Transfert mapping dans ScanDataResponseHelper)

--- src/Core/Api/ScanDataResponse.cs
+++ src/Core/Api/ScanDataResponse.cs
@@ -7,23 +7,10 @@ namespace RPLidar4Net.Core.Api
     /// 
     /// </summary>
     /// <remarks>LR001_SLAMTEC_rplidar_protocol_v2.1_en / p.16</remarks>
-    [StructLayout(LayoutKind.Sequential)]
     public struct ScanDataResponse
     {
-        public byte sync_quality;
-        public ushort angle_q6_checkbit;
-        public ushort distance_q2;
-
-        public ScanDataResponse(byte[] bytes)
-        {
-            //Byte0
-            sync_quality = bytes[0];
-            //Byte 1+2
-            angle_q6_checkbit = BitConverter.ToUInt16(bytes, 1);
-            //Byte 3+4
-            distance_q2 = BitConverter.ToUInt16(bytes, 3);
-            //5Bytes total
-        }
-
+        public byte SyncAndQuality { get; set; }
+        public ushort AngleQ6AndCheckbit { get; set; }
+        public ushort DistanceQ2 { get; set; }
     }
 }
--- src/Core/Api/ScanDataResponseHelper.cs
+++ src/Core/Api/ScanDataResponseHelper.cs
@@ -9,17 +9,20 @@ namespace RPLidar4Net.Core.Api
             if (bytes.Length < Constants.ScanDataResponseLength)
                 throw new Exception("RESULT_INVALID_ANS_TYPE");
 
-            ScanDataResponse scanDataResponse = new ScanDataResponse(bytes);
+            ScanDataResponse scanDataResponse = new ScanDataResponse();
+            scanDataResponse.SyncAndQuality = bytes[0];
+            scanDataResponse.AngleQ6AndCheckbit = BitConverter.ToUInt16(bytes, 1);
+            scanDataResponse.DistanceQ2 = BitConverter.ToUInt16(bytes, 3);
             return scanDataResponse;
         }
 
         public static Point ToPoint(ScanDataResponse scanDataResponse)
         {
             Point point = new Point();
-            point.Distance = scanDataResponse.distance_q2 / 4.0f;
-            point.Angle = (scanDataResponse.angle_q6_checkbit >> Constants.RPLIDAR_RESP_MEASUREMENT_ANGLE_SHIFT) / 64.0f;
-            point.Quality = (scanDataResponse.sync_quality >> Constants.RPLIDAR_RESP_MEASUREMENT_QUALITY_SHIFT);
-            int startFlag = (scanDataResponse.sync_quality & Constants.RPLIDAR_RESP_MEASUREMENT_SYNCBIT);
+            point.Distance = scanDataResponse.DistanceQ2 / 4.0f;
+            point.Angle = (scanDataResponse.AngleQ6AndCheckbit >> Constants.RPLIDAR_RESP_MEASUREMENT_ANGLE_SHIFT) / 64.0f;
+            point.Quality = (scanDataResponse.SyncAndQuality >> Constants.RPLIDAR_RESP_MEASUREMENT_QUALITY_SHIFT);
+            int startFlag = (scanDataResponse.SyncAndQuality & Constants.RPLIDAR_RESP_MEASUREMENT_SYNCBIT);
             point.StartFlag = (startFlag == 1);
             return point;
         }
