b5a6771d50875635a81359d4d22ad147685cd23a
Amael BERTEAU <amael.berteau@tech-easy.fr>
2020-02-26 19:01:19 +0100

    Ajout BitHelper

---
+++ src/Core.Tests/BitHelperTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Xunit;
+
+namespace RPLidar4Net.Core.Tests
+{
+    public class BitHelperTests
+    {
+        [Theory]
+        [InlineData(1, 8)]
+        [InlineData(2, 16)]
+        [InlineData(4, 32)]
+        public void GetBitLengthFromByteCountTheory(int byteCount, int expectedResult)
+        {
+            int result = BitHelper.GetBitLength(byteCount);
+            Assert.Equal(expectedResult, result);
+        }
+
+        [Theory]
+        [InlineData(0x00, 0x00, 0x0, 24)]
+        public void GetBitLengthFromByteArrayTheory(byte paramByte1, byte paramByte2, byte paramByte3, int expectedResult)
+        {
+            Int32 result = BitHelper.GetBitLength(new byte[]{paramByte1, paramByte2, paramByte3});
+            Assert.Equal(expectedResult, result);
+        }
+    }
+}
---
+++ src/Core/BitHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RPLidar4Net.Core
+{
+    public class BitHelper
+    {
+        public const int ByteLength = 8;
+        public const int UInt32Length = ByteLength * 4;
+
+        public static int GetBitLength(int byteCount)
+        {
+            return byteCount * ByteLength;
+        }
+
+        public static int GetBitLength(byte[] bytes)
+        {
+            int length = GetBitLength(bytes.Length);
+            return length;
+        }
+    }
+}
