3e7fb69288e1a484ab941dbcfede131d23219ae0
Amael BERTEAU <amael.berteau@tech-easy.fr>
2020-03-15 16:20:06 +0100

    NewScanEventArgs moved to RPLidar4Net.IO

--- src/IO/NewScanEventArgs.cs
+++ src/IO/NewScanEventArgs.cs
@@ -1,7 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using RPLidar4Net.Core;
 
-namespace RPLidar4Net.Core
+namespace RPLidar4Net.IO
 {
     public class NewScanEventArgs : EventArgs
     {
